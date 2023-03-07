3db0fbb6b0ac19a81b745acd4f4c40c1f121e55e
nandatritona <nandhafebrian3@gmail.com>
2023-03-07 11:37:28 +0700

    Membuat view Menampilkan Todolist

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -123,7 +123,26 @@ public class AplikasiTodoList {
      * Menampilkan view todo list
      */
     public static void viewShowTodoList(){
+        while (true) {
+            showTodoList();
 
+            System.out.println("Menu : ");
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("x. Kaluar");
+
+            var input  = input("Pilih");
+
+            if (input.equals("1")) {
+                viewAddTodoList();
+            } else if (input.equals("2")) {
+                viewRemoveTodoList();
+            } else if (input.equals("x")) {
+                break;
+            } else {
+                System.out.println("Pilihan tidak dimengerti");
+            }
+        }
     }
 
     /**
