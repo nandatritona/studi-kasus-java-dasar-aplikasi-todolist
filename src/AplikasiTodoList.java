aeb1d98ea7e15ec97bc21d198bf3cc778b9f2e79
nandatritona <nandhafebrian3@gmail.com>
2023-03-07 15:00:23 +0700

    Test keseluruhan app

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -2,7 +2,7 @@ public class AplikasiTodoList {
     public static String[] model = new String[10];
     public static java.util.Scanner scanner = new java.util.Scanner(System.in);
     public static void main(String[] args) {
-        testViewRemoveTodoList();
+        viewShowTodoList();
     }
 
     /**
