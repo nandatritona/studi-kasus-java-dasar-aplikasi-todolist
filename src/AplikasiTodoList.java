1efcef77e36df4f283a3b45fb0314716acedf22b
nandatritona <nandhafebrian3@gmail.com>
2023-03-07 11:00:28 +0700

    Membuat logic di remove todo list

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -68,8 +68,21 @@ public class AplikasiTodoList {
     /**
      * Mehapus todo dari list
      */
-    public static void removeTodoList() {
-
+    public static boolean removeTodoList(Integer number) {
+        if ((number - 1) >= model.length) {
+            return false;
+        } else if (model[number - 1] == null) {
+            return false;
+        } else {
+            for (int i = (number - 1); i < model.length; i++) {
+                if (i == (model.length) -1 ) {
+                    model[i] = null;
+                } else {
+                    model[i] = model[i + 1];
+                }
+            }
+            return true;
+        }
     }
 
     /**
